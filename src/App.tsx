5b829992ffb011f53a104fad4191fa8b7dbb75f6
Ronan LE PAGE <Rixdiz@MacBook-Pro-de-Claire.local>
2020-10-22 08:35:40 +0200

    wesh

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TinderCard from 'react-tinder-card';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = ['Film 1', 'Film 2', 'Film 3', 'Film 4', 'Film 5'];
+
+class App extends React.Component {
+
+  swiped = (direction: any, nameToDelete: string) => {
+    console.log('removing: ' + nameToDelete + ' from ' + direction)
+  }
+
+  outOfFrame = (name: string) => {
+    console.log(name + ' left the screen!')
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className='cardContainer'>
+
+          {data.map(movie =>
+            <TinderCard key={movie} onSwipe={(dir) => this.swiped(dir, movie)} onCardLeftScreen={() => this.outOfFrame(movie)}>
+              <div className='swipe'>
+                <div className='card'>{movie}</div>
+              </div>
+            </TinderCard>
+          )}
+
+        </div>
+      </div>
+
+    );
+  }
+
 }
 
 export default App;
